6bec8a999a672decc7a2ba0d26143f35c6740ed1
molmax <molmax@users.noreply.github.com>
2022-11-27 22:56:34 +0400

    Snake basic movement

--- src/main.js
+++ src/main.js
@@ -1,13 +1,46 @@
+class Snake {
+  constructor(x, y, direction) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  }
+
+  move() {
+    if (this.x > canvasWidth) {
+      this.x = 0;
+    }
+    if(this.y > canvasHeight) {
+      this.y = 0;
+    }
+    if (this.x < 0) {
+      this.x = canvasWidth;
+    } 
+    if(this.y < 0) {
+      this.y = canvasHeight;
+    }
+
+    if(this.direction === 'RIGHT') {
+      this.x = this.x + 5; 
+    } else if (this.direction === 'LEFT') {
+      this.x = this.x - 5; 
+    } else if (this.direction === 'UP') {
+      this.y = this.y - 5; 
+    } else if (this.direction === 'DOWN') {
+      this.y = this.y + 5; 
+    }
+    rect(this.x, this.y, 10);
+  }
+
+  changeDirection(direction) {
+    this.direction = direction;
+  }
+}
+
 const canvasWidth = 400;
 const canvasHeight = 400;
 const bckgColor = 120;
 const frameRateValue = 30;
-
-const cellSize = 10;
-const stepSize = 20;
-let snakeX = canvasWidth / 2;
-let snakeY = canvasHeight / 2;
-
+const snake = new Snake(canvasWidth / 2, canvasHeight / 2, 'RIGHT');
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -16,17 +49,17 @@ function setup() {
 
 function draw() {
   background(bckgColor);
-  rect(snakeX, snakeY, cellSize);
+  snake.move();
 }
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    snakeY = snakeY - stepSize;
+    snake.changeDirection('UP');
   } else if (keyCode === DOWN_ARROW) {
-    snakeY = snakeY + stepSize;
+    snake.changeDirection('DOWN');
   } else if (keyCode === LEFT_ARROW) {
-    snakeX = snakeX - stepSize;
+    snake.changeDirection('LEFT');
   } else if (keyCode === RIGHT_ARROW) {
-    snakeX = snakeX + stepSize;
+    snake.changeDirection('RIGHT');
   }
 }
