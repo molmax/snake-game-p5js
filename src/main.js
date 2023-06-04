0f380f43c1fe34ccda3e34ad8b706dedacbc8326
molmax <gr33nengineer@gmail.com>
2023-06-04 19:12:15 +0300

    Implemented collision logic, consuming apple and end game screen

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-cHeight = 400;
-cWidth = 400;
-  
+cHeight = 800;
+cWidth = 800;
+score = 0;
+
 function setup() {
     createCanvas(cHeight, cWidth);
     frameRate(5);
@@ -15,18 +16,38 @@ function setup() {
         y = Math.floor(random(cHeight));
     } while (y % gridSize != 0);
 
-    apple = createVector(x, y);
+    apple = createApple();
   }
   
   function draw() {
     background(60)
-    sHead = snake.cells[snake.length - 1];
-    // if (sHead.x) {
-        // onAppleConsumed();
-    // }
+
+    sHead = snake.cells[0];
+    if (sHead.x == apple.x && sHead.y == apple.y) {
+        onAppleConsumed();
+    }
     snake.update();
+    if (snake.checkCollision()) {
+        endGame();
+        return;
+    }
     snake.render();
-    this.renderApple();
+    renderApple();
+    drawScore();
+  }
+
+  function createApple() {
+    do {
+        x = Math.floor(random(cWidth));
+    } while (x % gridSize != 0);
+
+    do {
+        y = Math.floor(random(cHeight));
+    } while (y % gridSize != 0);
+
+    apple = { x: x, y: y };
+
+    return apple;
   }
 
   function renderApple() {
@@ -35,7 +56,12 @@ function setup() {
   }
 
   function onAppleConsumed() {
-    
+    apple = createApple();
+    lastCell = snake.cells[snake.cells.length-1];
+    x = lastCell.x - gridSize;
+    y = lastCell.y = gridSize;
+    snake.cells.push({x: x, y: y});
+    score++;
   }
   
   function keyPressed() {
@@ -96,12 +122,50 @@ function setup() {
                 prevY = tempY;
             }        
         });
+        
     }
 
     render() {
         this.cells.forEach(cell => {
             fill(0, 180, 0);
             rect(cell.x, cell.y, gridSize);
-        })
+        });
     }
+
+    checkCollision() {
+        let isCollision = false;
+        sHead = snake.cells[0];
+
+        if (sHead.x >= cWidth || sHead.x <= 0 || sHead.y >= cWidth || sHead.y <= 0) {
+            isCollision = true;
+        }
+
+        let tail = snake.cells.slice();
+        tail.shift();
+
+        tail.forEach(cell => {
+            if (sHead.x == cell.x && sHead.y == cell.y) {
+                isCollision =  true;
+            }
+        });
+        return isCollision;
+    }
+}
+
+function drawScore() {
+    textSize(20);
+    fill(10, 200, 10);  
+    text('score: ' + this.score, cWidth - 80, 20);
+}
+
+function endGame() {
+    textSize(32);
+    fill(256, 256, 256);  
+    text('Game Over!', cWidth / 2 - 100, cHeight / 2);
+
+    textSize(20);
+    fill(200, 10, 10);  
+    text('Score: ' + this.score, cWidth / 2 - 50, cHeight / 2 + 30);
+ 
+    noLoop();
 }
